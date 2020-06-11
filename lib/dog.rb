016170095992578badfd83e330adf0f00322b9c6
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-11 15:39:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,21 @@
 class Dog
 
-  def name=(dogs_name)
-    @name = dogs_name
-  end
+  attr_accessor :name, :breed
 
-  def name
-    @name
-  end
-
-  def breed=(dogs_breed)
-    @breed = dogs_breed
-  end
-
-  def breed
-    @breed
-  end
+  # def name=(dogs_name)
+  #   @name = dogs_name
+  # end
+  #
+  # def name
+  #   @name
+  # end
+  #
+  # def breed=(dogs_breed)
+  #   @breed = dogs_breed
+  # end
+  #
+  # def breed
+  #   @breed
+  # end
 
 end
